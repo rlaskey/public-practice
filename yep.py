feb9d0a10591f556b3d6c8f7dd38965c42a0ef87
Richard Laskey <rlaskey@outlook.com>
2025-04-02 11:31:54 -0400

    add jump game ii

--- yep.py
+++ yep.py
@@ -36,6 +36,7 @@ def test_rev() -> None:
 
 def parens(n: int) -> List[str]:
     # generate all possible pairs of `n` parentheses
+
     result = []
 
     def f(partial: str, o: int, c: int):
@@ -71,6 +72,7 @@ def test_parens() -> None:
 
 def jump(nums: List[int]) -> bool:
     # Jump Game: can you get to the end?
+
     right = len(nums) - 1
     for i in range(len(nums) - 2, -1, -1):
         if right <= i + nums[i]:
@@ -84,7 +86,33 @@ def test_jump() -> None:
     assert jump([3, 2, 1, 0, 4]) == False
 
 
+def jump_ii(nums: List[int]) -> int:
+    # minimum number of jumps to get to the end
+
+    result = left = farthest = 0
+    for i in range(len(nums) - 1):
+        from_here = i + nums[i]
+        if from_here > farthest:
+            farthest = from_here
+
+        if i == left:
+            result += 1
+            left = farthest
+            if left >= len(nums) - 1:
+                break
+
+    return result
+
+
+def test_jump_ii() -> None:
+    assert jump_ii([3, 2, 1, 1, 4]) == 2
+    assert jump_ii([4, 0, 0, 0, 4]) == 1
+    assert jump_ii([1, 1]) == 1
+    assert jump_ii([1]) == 0
+
+
 if __name__ == "__main__":
     test_rev()
     test_parens()
     test_jump()
+    test_jump_ii()
